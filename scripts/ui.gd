7e71d7847e0c4453d0f32641397651446541660e
lucasfs7 <me@lucasfsouza.com.br>
2021-05-16 18:45:22 -0300

    star the game paused

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -10,16 +10,22 @@ onready var score := $Score as Label
 func _ready():
 	PlayerStats.connect("score_changed", self, "_set_score_text")
 
+	if PlayerStats.first_run:
+		_pause_game()
 
 func _process(delta):
 	if Input.is_action_just_pressed("game_pause"):
-		get_tree().paused = !get_tree().paused
-
-		if get_tree().paused:
-			menu.visible = true
-		else:
-			menu.visible = false
+		_pause_game()
 
 
 func _set_score_text(value):
 	score.text = SCORE_TEXT % value
+
+
+func _pause_game():
+	get_tree().paused = !get_tree().paused
+
+	if get_tree().paused:
+		menu.visible = true
+	else:
+		menu.visible = false
